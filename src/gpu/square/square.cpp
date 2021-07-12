b9976264fcf6624a40a9b758ae19b6d6b64e9dda
Kyle Roarty <kyleroarty1716@gmail.com>
2021-07-12 18:42:39 -0500

    resources: Update square for ROCm 4 and dGPU support
    ROCm 4 made it so previous methods of GPU memory management
    used in ROCm 1.6 don't work in gem5. To get around that, we
    use hipHostMalloc, which allocates host and device accessible
    memory at the same time, all in a single pointer.
    
    Additionally, because APU and dGPU code now don't allocate
    memory differently, the #DGPU define is removed.
    
    Finally, the Makefile is updated to build all supported GPU
    versions into the same binary.
    
    Change-Id: I89dd2403da0181f53961f9c05f93f46c087603a8
    Reviewed-on: https://gem5-review.googlesource.com/c/public/gem5-resources/+/47979
    Reviewed-by: Matthew Poremba <matthew.poremba@amd.com>
    Reviewed-by: Matt Sinclair <mattdsinclair@gmail.com>
    Reviewed-by: Alex Dutu <alexandru.dutu@amd.com>
    Reviewed-by: Bobby R. Bruce <bbruce@ucdavis.edu>
    Maintainer: Matt Sinclair <mattdsinclair@gmail.com>
    Maintainer: Bobby R. Bruce <bbruce@ucdavis.edu>
    Tested-by: Bobby R. Bruce <bbruce@ucdavis.edu>

--- src/gpu/square/square.cpp
+++ src/gpu/square/square.cpp
@@ -50,9 +50,6 @@ vector_square(T *C_d, const T *A_d, size_t N)
 
 int main(int argc, char *argv[])
 {
-#ifdef DGPU
-    float *A_d, *C_d;
-#endif
     float *A_h, *C_h;
     size_t N = 1000000;
     size_t Nbytes = N * sizeof(float);
@@ -63,38 +60,21 @@ int main(int argc, char *argv[])
     #ifdef __HIP_PLATFORM_HCC__
       printf ("info: architecture on AMD GPU device is: %d\n",props.gcnArch);
     #endif
-    printf ("info: allocate host mem (%6.2f MB)\n", 2*Nbytes/1024.0/1024.0);
-    A_h = (float*)malloc(Nbytes);
-    CHECK(A_h == 0 ? hipErrorMemoryAllocation : hipSuccess );
-    C_h = (float*)malloc(Nbytes);
-    CHECK(C_h == 0 ? hipErrorMemoryAllocation : hipSuccess );
+    printf ("info: allocate host and device mem (%6.2f MB)\n", 2*Nbytes/1024.0/1024.0);
+    CHECK(hipHostMalloc(&A_h, Nbytes));
+    CHECK(hipHostMalloc(&C_h, Nbytes));
     // Fill with Phi + i
     for (size_t i=0; i<N; i++)
     {
         A_h[i] = 1.618f + i;
     }
 
-#ifdef DGPU
-    printf ("info: allocate device mem (%6.2f MB)\n", 2*Nbytes/1024.0/1024.0);
-    CHECK(hipMalloc(&A_d, Nbytes));
-    CHECK(hipMalloc(&C_d, Nbytes));
-
-    printf ("info: copy Host2Device\n");
-    CHECK ( hipMemcpy(A_d, A_h, Nbytes, hipMemcpyHostToDevice));
-#endif
-
     const unsigned blocks = 512;
     const unsigned threadsPerBlock = 256;
 
     printf ("info: launch 'vector_square' kernel\n");
-#ifdef DGPU
-    hipLaunchKernelGGL(vector_square, dim3(blocks), dim3(threadsPerBlock), 0, 0, C_d, A_d, N);
-
-    printf ("info: copy Device2Host\n");
-    CHECK ( hipMemcpy(C_h, C_d, Nbytes, hipMemcpyDeviceToHost));
-#else
     hipLaunchKernelGGL(vector_square, dim3(blocks), dim3(threadsPerBlock), 0, 0, C_h, A_h, N);
-#endif
+    hipDeviceSynchronize();
 
     printf ("info: check result\n");
     for (size_t i=0; i<N; i++)  {
