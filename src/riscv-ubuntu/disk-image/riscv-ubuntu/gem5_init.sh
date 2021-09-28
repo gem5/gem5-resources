b3f65a6d78226b070391040394f2cc79480dcd42
Hoa Nguyen <hoanguyen@ucdavis.edu>
2021-09-27 18:20:30 -0700

    resources: Add riscv-ubuntu disk image
    Signed-off-by: Hoa Nguyen <hoanguyen@ucdavis.edu>
    Change-Id: I6e6c30df6a3ccb1991e66c4384b38dd5df0e4dfc
    Reviewed-on: https://gem5-review.googlesource.com/c/public/gem5-resources/+/51028
    Reviewed-by: Bobby R. Bruce <bbruce@ucdavis.edu>
    Maintainer: Bobby R. Bruce <bbruce@ucdavis.edu>
    Tested-by: Bobby R. Bruce <bbruce@ucdavis.edu>

---
+++ src/riscv-ubuntu/disk-image/riscv-ubuntu/gem5_init.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Copyright (c) 2021 The University of Texas at Austin.
+# SPDX-License-Identifier: BSD 3-Clause
+
+echo "Starting gem5 init... reading run script file."
+if ! m5 readfile > /tmp/script; then
+    echo "Failed to run m5 readfile, exiting!"
+    rm -f /tmp/script
+    if ! m5 exit; then
+        # Useful for booting the disk image in (e.g.,) qemu for debugging
+        echo "m5 exit failed, dropping to shell."
+        /bin/sh
+    fi
+else
+    echo "Running m5 script from /tmp/script"
+    chmod 755 /tmp/script
+    /tmp/script
+    echo "Done running script, exiting."
+    rm -f /tmp/script
+    m5 exit
+fi
