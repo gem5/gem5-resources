e06b0b9225585020cb7fb84dfc142ed5fa6a9f7d
Kaustav Goswami <kggoswami@ucdavis.edu>
2021-10-30 00:27:15 -0700

    resources: Fixes re-building SPEC CPU2017 benchmarks.
    This change fixes the issue with running SPEC CPU2017 benchmarks
    via gem5. It provides a valid path to /usr/bin in the disk-image
    during execution and avoids re-building the same benchmark
    programs.
    
    Change-Id: Ice2ec9bbf52416558b8fdb5f3f37d501274cf141
    Reviewed-on: https://gem5-review.googlesource.com/c/public/gem5-resources/+/52243
    Reviewed-by: Jason Lowe-Power <power.jg@gmail.com>
    Reviewed-by: Bobby R. Bruce <bbruce@ucdavis.edu>
    Maintainer: Jason Lowe-Power <power.jg@gmail.com>
    Maintainer: Bobby R. Bruce <bbruce@ucdavis.edu>
    Tested-by: Jason Lowe-Power <power.jg@gmail.com>
    Tested-by: Bobby R. Bruce <bbruce@ucdavis.edu>

--- src/spec-2017/disk-image/spec-2017/runscript.sh
+++ src/spec-2017/disk-image/spec-2017/runscript.sh
@@ -19,7 +19,7 @@ if [ -s workloads ]; then
 
     # run the commands
     read -r workload size m5filespath < workloads
-    runcpu --size $size --iterations 1 --config myconfig.x86.cfg --noreportable --nobuild $workload
+    runcpu --size $size --iterations 1 --config myconfig.x86.cfg --define gcc_dir="/usr" --noreportable --nobuild $workload
     m5 exit
 
     # copy the SPEC result files to host
