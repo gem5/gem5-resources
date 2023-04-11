4309ab37bd94a5b0790673901574317551350823
Roger Chang <rogerycchang@google.com>
2023-04-11 15:02:01 +0800

    resources: Update asmtest revision
    Update asmtest base revision
    from e65ecdf941a5484af27f9be223fb655ebcb0398b
    to 394937eceb62bc4ba8ad5a4e916f0508d9803229
    
    Change-Id: I85defce4ffb5f9a57c5da486e8503f7619ce7af0
    Reviewed-on: https://gem5-review.googlesource.com/c/public/gem5-resources/+/68658
    Reviewed-by: Bobby Bruce <bbruce@ucdavis.edu>
    Tested-by: Bobby Bruce <bbruce@ucdavis.edu>
    Maintainer: Bobby Bruce <bbruce@ucdavis.edu>

--- src/asmtest/env/p/riscv_test.h
+++ src/asmtest/env/p/riscv_test.h
@@ -107,6 +107,13 @@
   .align 2;                                                             \
 1:
 
+#define INIT_RNMI                                                       \
+  la t0, 1f;                                                            \
+  csrw mtvec, t0;                                                       \
+  csrwi CSR_MNSTATUS, MNSTATUS_NMIE;                                    \
+  .align 2;                                                             \
+1:
+
 #define INIT_SATP                                                      \
   la t0, 1f;                                                            \
   csrw mtvec, t0;                                                       \
@@ -153,6 +160,8 @@
 #define EXTRA_TVEC_MACHINE
 #define EXTRA_INIT
 #define EXTRA_INIT_TIMER
+#define FILTER_TRAP
+#define FILTER_PAGE_FAULT
 
 #define INTERRUPT_HANDLER j other_exception /* No interrupts should occur */
 
@@ -206,6 +215,7 @@ handle_exception:                                                       \
 reset_vector:                                                           \
         INIT_XREG;                                                      \
         RISCV_MULTICORE_DISABLE;                                        \
+        INIT_RNMI;                                                      \
         INIT_SATP;                                                      \
         INIT_PMP;                                                       \
         DELEGATE_NO_TRAPS;                                              \
@@ -275,8 +285,8 @@ tohost_data:                                                            \
 #define RVTEST_DATA_BEGIN                                               \
         EXTRA_DATA                                                      \
         .pushsection .tohost,"aw",@progbits;                            \
-        .align 6; .global tohost; tohost: .dword 0;                     \
-        .align 6; .global fromhost; fromhost: .dword 0;                 \
+        .align 6; .global tohost; tohost: .dword 0; .size tohost, 8;    \
+        .align 6; .global fromhost; fromhost: .dword 0; .size fromhost, 8;\
         .popsection;                                                    \
         .align 4; .global begin_signature; begin_signature:
 
