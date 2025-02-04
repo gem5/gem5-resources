8459cdf922246e0cdb46dd1d05b67675938a0307
Harshil Patel <harshilp2107@gmail.com>
2025-02-04 12:17:07 -0800

    resources: Add hypercall for x86 base disk image

--- src/ubuntu-generic-diskimages/files/x86/gem5_init.sh
+++ src/ubuntu-generic-diskimages/files/x86/gem5_init.sh
@@ -19,7 +19,7 @@ no_systemd=false
 # This will cause the simulation to exit. Note that this will
 # cause qemu to fail.
 printf "Kernel booted, In gem5 init...\n"
-gem5-bridge exit # TODO: Make this a specialized event.
+gem5-bridge hypercall 1
 
 if [[ $cmdline == *"no_systemd"* ]]; then
     no_systemd=true
